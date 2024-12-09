cd12211e8d3fcba528c887b7401890fd21d53592
you@example.com <username>
2024-12-09 14:40:40 +0800

    Site updated: 2024-12-09 14:40:39

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","d4abe52f26061944a4496497b0002a68"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
